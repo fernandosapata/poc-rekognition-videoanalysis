54ac97c919eeaebc9255daea331964cd357895c6
Tatiana Morimoto <tmmorimoto@gmail.com>
2017-01-02 23:38:34 +0000

    Merge remote-tracking branch 'origin/lambda_inventory_technique' into tatiana
    # Conflicts:
    #	05-Sourcecode/03-Frontend/dev/app/controllers/maincontroller.js

--- 05-Sourcecode/03-Frontend/dev/app/controllers/maincontroller.js
+++ 05-Sourcecode/03-Frontend/dev/app/controllers/maincontroller.js
@@ -4,47 +4,44 @@ var AWS = require('aws-sdk');
 
 app.controller('rekognition', ['$scope', 'AWSIotWebsocket', 'AWSS3', function($scope, AWSIotWebsocket, AWSS3) {
 
-    $scope.uploadFile = function(){
-        //Usage of AWSS3
-        AWSS3.init();
-        AWSS3.uploadFile(
-            document.getElementById('uploadFile'),
-            "test/",
-            function(err, data) {
-                if (err) {
-                  return alert('There was an error uploading your file: ', err.message);
-                }
-//                alert('Successfully uploaded file.');
-                $scope.loadingStart = true;
-                $scope.loading =  20;
-                $scope.message = "Successfully uploaded video";
-                $scope.$apply();
-            }
-        );
-
+    $scope.init = function(){
         //Usage of AWSIotWebsocket
         AWSIotWebsocket.init(
             function() {
                console.log('connect');
-               //
-               // Subscribe to our current topic.
-               //
                AWSIotWebsocket.subscribeToPrivateTopic();
             },
             function() {
                console.log('reconnect');
             },
             function(topic, payload) {
-                console.log('message: ' + topic + ':' + payload.toString());
-                var data = {status: []},
-                string = payload.toString(),
-                json = JSON.parse(string),
-                push = data.status.push(json);
-                $scope.message = data.status[0].message;
-                $scope.loading = data.status[0].percentage;
-                $scope.$apply();
+                payload_string = payload.toString();
+                console.log('message: ' + topic + ':' + payload_string);
+                json = JSON.parse(payload_string);
+                if(json.type == 'status'){
+                    $scope.message = json.payload.message;
+                    $scope.loading = json.payload.percentage;
+                    $scope.$apply();
+                }
             }
         );
         AWSIotWebsocket.setupWebSocket();
+    }
+
+    $scope.uploadFile = function(){
+        $scope.loadingStart = true;
+        //Usage of AWSS3
+        AWSS3.init();
+        AWSS3.uploadFile(
+            document.getElementById('uploadFile'),
+            "tmp/",
+            function(err, data) {
+                if (err) {
+                    $scope.loadingStart = false;
+                    return alert(err.message);
+                }
+            }
+        );
+
     };
 }]);
