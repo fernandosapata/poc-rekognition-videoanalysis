796c587c848b8065015cc5d62eadf76b2eb446d0
Paulo Almeida <mpaulo@amazon.com>
2017-01-05 16:26:53 -0800

    Fix issue with DynamoDB Streams

--- 05-Sourcecode/01-Server/05-RVA_process_dynamodbstream_function/lambda_function.py
+++ 05-Sourcecode/01-Server/05-RVA_process_dynamodbstream_function/lambda_function.py
@@ -18,44 +18,45 @@ def lambda_handler(event, context):
     results_array=[]
 
     for record in event['Records']:
-        row = record['dynamodb']['NewImage']
-        iot_topic=row['Topic']['S']
+        if record['eventName'] != 'REMOVE':
+            row = record['dynamodb']['NewImage']
+            iot_topic=row['Topic']['S']
 
-        # Check if we have already seen this iot topic to make sure we won't mess with other simutaneous process.
-        element = None
-        for v in results_array:
-            if v['iot_topic'] == iot_topic:
-                element = v
-                break
-        if element is None:
-            element = {'iot_topic':iot_topic, 'number_of_items':0, 'max_completed_items':0}
-            results_array.append(element)
+            # Check if we have already seen this iot topic to make sure we won't mess with other simutaneous process.
+            element = None
+            for v in results_array:
+                if v['iot_topic'] == iot_topic:
+                    element = v
+                    break
+            if element is None:
+                element = {'iot_topic':iot_topic, 'number_of_items':0, 'max_completed_items':0}
+                results_array.append(element)
 
-        local_sum=0
-        part_dict=row['Parts']['M']
-        element['number_of_items']=len(part_dict)
-        for key, value in part_dict.iteritems():
-            if value['S'] == "COMPLETED":
-                local_sum+=1
-        if local_sum > element['max_completed_items']:
-            element['max_completed_items'] = local_sum
+            local_sum=0
+            part_dict=row['Parts']['M']
+            element['number_of_items']=len(part_dict)
+            for key, value in part_dict.iteritems():
+                if value['S'] == "COMPLETED":
+                    local_sum+=1
+            if local_sum > element['max_completed_items']:
+                element['max_completed_items'] = local_sum
 
-        if element['max_completed_items'] == 0:
-            #  it means this is a fresh record
-            videos_results_table.put_item(Item={
-                'Identifier': row['Identifier']['S'],
-                'FaceDetails': {
-                    'Smile' : {'Positive': 0, 'Negative': 0},
-                    'Eyeglasses' : {'Positive': 0, 'Negative': 0},
-                    'Sunglasses' : {'Positive': 0, 'Negative': 0},
-                    'Gender' : {'Male': 0, 'Female': 0},
-                    'Beard' : {'Positive': 0, 'Negative': 0},
-                    'Mustache' : {'Positive': 0, 'Negative': 0},
-                    'EyesOpen' : {'Positive': 0, 'Negative': 0},
-                    'MouthOpen' : {'Positive': 0, 'Negative': 0},
-                    'Emotions' : {'HAPPY': 0, 'SAD': 0, 'ANGRY': 0, 'DISGUSTED': 0, 'CONFUSED': 0, 'SURPRISED': 0}
-                }
-            })
+            if element['max_completed_items'] == 0:
+                #  it means this is a fresh record
+                videos_results_table.put_item(Item={
+                    'Identifier': row['Identifier']['S'],
+                    'FaceDetails': {
+                        'Smile' : {'Positive': 0, 'Negative': 0},
+                        'Eyeglasses' : {'Positive': 0, 'Negative': 0},
+                        'Sunglasses' : {'Positive': 0, 'Negative': 0},
+                        'Gender' : {'Male': 0, 'Female': 0},
+                        'Beard' : {'Positive': 0, 'Negative': 0},
+                        'Mustache' : {'Positive': 0, 'Negative': 0},
+                        'EyesOpen' : {'Positive': 0, 'Negative': 0},
+                        'MouthOpen' : {'Positive': 0, 'Negative': 0},
+                        'Emotions' : {'HAPPY': 0, 'SAD': 0, 'ANGRY': 0, 'DISGUSTED': 0, 'CONFUSED': 0, 'SURPRISED': 0}
+                    }
+                })
 
     for element in results_array:
         calculated_value = int(round(initial_percentage + (((final_percentage-initial_percentage)/element['number_of_items'])*element['max_completed_items'])))
